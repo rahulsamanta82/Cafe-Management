89f48345407628f57f2d2d317eec4119993a218c
Rahul Samanta <145849509+rahulsamanta82@users.noreply.github.com>
2026-02-20 19:28:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,35 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
 
-// https://vitejs.dev/config/
-export default function defineConfig() {
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src')
-      }
+// // https://vitejs.dev/config/
+// export default function defineConfig() {
+//   return {
+//     plugins: [react()],
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, './src')
+//       }
+//     },
+//     optimizeDeps: {
+//       exclude: ['lucide-react'],
+//     },
+//   };
+// }
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/",   // ✅ VERY IMPORTANT FOR VERCEL
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    optimizeDeps: {
-      exclude: ['lucide-react'],
-    },
-  };
-}
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+})
